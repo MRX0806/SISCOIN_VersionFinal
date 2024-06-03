23de1998094bc925ad5e2d743733ea6a4e65cbba
Sandra <lucia020604@gmail.com>
2024-06-03 23:43:09 +0200

    reporte_ad

---
+++ ADMINISTRADOR/reporte.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reporte.css">
+    <title>Reporte</title>
+   
+</head>
+<body>
+    <main>
+        <div class=titulo>
+            <h1>Reporte</h1>
+        </div>
+        <article>
+            <div class="wrapper">
+                <div class="one">
+                    <h3>Temas nuevos</h3>
+                    <img src="log1.png" alt=""> 
+                <?php
+                    include 'temas.php';
+                ?>
+                </div>
+
+                <div class="two">
+                    <h3>Informes</h3>
+                    <img src="log2.png" alt=""> 
+                <?php
+                    include 'informes.php';
+                ?>
+                </div>
+                <div class="three">
+                    <h3>Grupos</h3>
+                    <img src="log3.png" alt=""> 
+                <?php
+                    include 'grupo.php';
+                ?>
+                </div>
+            </div>
+        </article>
+        
+    </main>
+</body>
+</html>
