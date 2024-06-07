806739698e82a6aa444dae1ad44cb03f6c784ec4
Angel Diaz <lpalominodi@ucvvirtual.edu.pe>
2024-06-07 17:58:11 -0500

    Mejorando Design

--- conexion.php
+++ conexion.php
@@ -4,11 +4,14 @@
     $USUARIO = "root"; 
     $PASS = "";
 
-    try {
-        $pdo = new PDO("mysql:host=$SERVER;dbname=$BDNAME", $USUARIO, $PASS);
-        // Set the PDO error mode to exception
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "No se pudo acceder a la BD: " . $e->getMessage(); //me indica el error que capturo en caso de que el try no funcione
-    }
-
+    try 
+        {
+            $pdo = new PDO("mysql:host=$SERVER;dbname=$BDNAME", $USUARIO, $PASS);
+           
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexión exitosa a la base de datos";//Verifica si hubo conexion
+        } catch (PDOException $e) 
+            {
+                echo "No se pudo acceder a la BD: " . $e->getMessage();
+            }
+?>
