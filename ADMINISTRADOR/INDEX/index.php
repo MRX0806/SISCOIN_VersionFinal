85e9b9f3b85e11ea9a4346a86b13b5dfb23e8352
Sandra <lucia020604@gmail.com>
2024-06-07 23:38:29 +0200

    enca_adm

---
+++ ADMINISTRADOR/INDEX/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Administrador</title>
+    <link rel="stylesheet" href="../../USUARIO_CLIENTE/header_Nav/Header.css">
+    <link rel="stylesheet" href="index.css">
+
+</head>
+<body>
+    <header>
+        <div class="header-degradado">
+            <div class="center">
+                <h1>SISCOIN</h1>
+                <h3>Sistema Colaborativo para la Investigación</h3>
+            </div>
+            <div class="btn">
+                <input type="submit" value="Iniciar Sesion" class="estilo-btn ini">
+                <!-- hols -->
+            </div>
+        </div>
+    </header>
+</body>
+</html>
