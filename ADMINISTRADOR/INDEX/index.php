d071207ad2d279a911c8cc88f3a2bf8e510e8648
Sandra <lucia020604@gmail.com>
2024-06-08 03:05:48 +0200

    agregandoinfo_indexadmi

--- ADMINISTRADOR/INDEX/index.php
+++ ADMINISTRADOR/INDEX/index.php
@@ -17,9 +17,48 @@
             </div>
             <div class="btn">
                 <input type="submit" value="Iniciar Sesion" class="estilo-btn ini">
-                <!-- hols -->
             </div>
         </div>
     </header>
+
+
+    <article>
+        <section class="relleno">
+            <div class="contenido">
+                <h2>Bienvenido al Panel de Administración</h2>
+                <p>Somos un grupo de estudiantes de Ingeniería de Sistemas comprometidos con la mejora de la colaboración académica. Desde la administración, nuestra misión es mantener la plataforma segura y eficiente para todos los usuarios.</p>
+            </div>
+                <img src="../../img/ad1.png" alt="imaadmi" class="ima1">
+        </section>
+        
+        <section class="puntos container">
+            <div class="inf1">
+                <!-- <img src="../img/nina3.png" alt="nin1"> -->
+                <div class="text-info">
+                    <h3>¿Quiénes somos?</h3>
+                    <p>Somos un grupo de estudiantes de Ingeniería de Sistemas comprometidos con la mejora de la colaboración académica. Desde la administración, nuestra misión es mantener la plataforma segura y eficiente para todos los usuarios.</p>
+                </div>
+            </div>
+            <div class="inf1">
+                <!-- <img src="../img/nina2.png" alt="nin1"> -->
+                <div class="text-info">
+                    <h3>¿Qué queremos?</h3>
+                    <p>En el rol de administrador, gestionamos usuarios, proyectos y recursos para asegurar que la plataforma funcione sin problemas y cumpla con sus objetivos de colaboración y apoyo a la investigación.</p>
+                </div>
+            </div>
+            <div class="inf1">
+                <!-- <img src="../img/nina1.png" alt="nin1"> -->
+                <div class="text-info">
+                    <h3>Nuestra mision</h3>
+                    <p>Empoderar a los estudiantes universitarios proporcionando las herramientas y la red necesarias para realizar investigaciones de alta calidad. Facilitar la colaboración y el acceso a recursos confiables es esencial para avanzar en el conocimiento y la innovación.</p>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <!-- hola:v -->
+            
+        </footer>
+    </article>
+
 </body>
 </html>
