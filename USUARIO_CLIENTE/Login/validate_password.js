6ca24bd498e0e03221268b8f2aee6c08bd899401
Luis Angel Palomino Diaz <lpalominodi@uccvirtual.edu.pe>
2024-06-08 21:17:26 -0500

    Eliminado_OG_Login

--- USUARIO_CLIENTE/Login/validate_password.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    const newPassword = document.getElementById('new_password').value;
-    const confirmPassword = document.getElementById('confirm_password').value;
-    if (newPassword !== confirmPassword) {
-        alert("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.");
-        return false;
-    }
-    return true;
-}
