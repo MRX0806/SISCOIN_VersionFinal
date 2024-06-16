e423cdb1e34798d142912fa40c7fff95231899e9
MRX0806 <minie080619@gmail.com>
2024-06-15 21:47:01 -0500

    AgregandoMysqli

--- conexion.php
+++ conexion.php
@@ -4,6 +4,7 @@
     $USUARIO = "root"; 
     $PASS = "";
 
+    /*PDO*/
     try {
         $pdo = new PDO("mysql:host=$SERVER;dbname=$BDNAME", $USUARIO, $PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,4 +12,16 @@
     } catch (Exception $e) {
         echo "No se pudo conectar a la base de datos: " . $e->getMessage();
     }
+
+    /* MYSQLI
+    try {
+        $mysqli = new mysqli($SERVER, $USUARIO, $PASS, $BDNAME);
+        if ($mysqli->connect_errno) {
+            throw new Exception("No se pudo conectar a la base de datos: " . $mysqli->connect_error);
+        }
+        echo "Conexión exitosa";
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    */
 ?>
